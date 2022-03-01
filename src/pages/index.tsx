48db1ea8b6ed54aeefc783d207aa29f92b290f30
André Luiz Alves Costa <andrelac963@gmail.com>
2022-02-28 22:23:36 -0300

    Dev: configurações iniciais do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Flex } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <h1>Helo</h1>
+    </Flex>
+  )
+}
